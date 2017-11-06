4fd810dddc90277c1e6c9db32599a564b069ff89
Sanyam Gupta <jsanyam4@gmail.com>
2017-11-06 01:36:55 +0000

    Sanyam 1 git

---
+++ git.c
@@ -0,0 +1,9 @@
+#include"stdio.h"
+int main(void)
+{
+    float x,*p;
+    p=&x;
+    printf("Enter the value:\n");
+    scanf("%f",p);
+    printf("printing value and address using pointer x=%f,address=%p\n",*p,p);
+}
