eb0f944b59272ef5b01a4f7ca7473961a654565f
Sanyam Gupta <jsanyam4@gmail.com>
2017-11-06 02:26:00 +0000

    fibonacci

--- git.c
+++ git.c
@@ -1,9 +1,30 @@
-#include"stdio.h"
+#include<stdio.h>
+#define size 50
+void fibonacci(unsigned int *p);
 int main(void)
 {
-    float x,*p;
-    p=&x;
-    printf("Enter the value:\n");
-    scanf("%f",p);
-    printf("printing value and address using pointer x=%f,address=%p\n",*p,p);
+	unsigned int n;
+	unsigned int *p=&n;
+	printf("Enter the range:\n");
+	scanf("%i",&n);
+	fibonacci(p);
+	printf("\n");
+	return 0;
+}
+void fibonacci(unsigned int *p)
+{
+	unsigned int n=*p;
+	unsigned int a[size];
+	a[0]=0;
+	a[1]=1;
+	for(int i=1;i<=n;i++)
+	{
+		a[i+1]=a[i]+a[i-1];
+
+	}
+	printf("The fibonacci sequence is:\n");
+	for(int j=1;j<=n;j++)
+	{
+		printf("%u, %i\n",a[j],a[j]%7);
+	}
 }
